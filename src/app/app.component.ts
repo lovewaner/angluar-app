635e160d430311d17d3ae4b04cb11bc2f10484e4
qingnilaichi <qing474973903@163.com>
2017-09-29 17:22:17 +0800

    路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
  selector: 'app-root',
  template: `
-   <h1>{{title}}</h1>
-   <my-heroes></my-heroes>
- `
+   <h1>{{ title }}</h1>
+   <nav>
+    <a routerLink="/dashboard">Dashboard</a>
+    <a routerLink="/heroes">heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+   `
 })
 export class AppComponent {
- title = 'Tour of Heroes';
+  title = 'Tour of Heroes'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,26 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule } from '@angular/router';
  
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent }     from './dashboard.component';
 import { HeroService }         from './hero.service';
 import { AppComponent } from './app.component';
  
-RouterModule.forRoot([
-  {
-    path: 'heroes',
-    component: HeroDetailComponent,
-  }
-])
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent,
   ],
   providers: [
     HeroService
